fb5d2bd3d1b5bf6d7de30026c4b5c0022421f47d
Tang <1194593491@qq.com>
2024-08-07 00:18:44 +0800

    学习gorm创建功能

--- gorm-study/create/main.go
+++ gorm-study/create/main.go
@@ -5,10 +5,14 @@ import (
 	"github.com/Tzz1194593491/gorm-study/model"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("./test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("./test.db"), &gorm.Config{
+		Logger:          logger.Default.LogMode(logger.Info),
+		CreateBatchSize: 2,
+	})
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -17,8 +21,9 @@ func main() {
 
 	createSingle(db)
 	createMany(db)
-
 	createBySomeFields(db)
+	createUseHook(db)
+	createByMap(db)
 }
 
 // 创建数据
@@ -29,17 +34,24 @@ func createSingle(db *gorm.DB) {
 		Password: "123",
 	}
 	result := db.Create(user)
-	fmt.Printf("%+v", result)
+	fmt.Printf("%+v\n", result)
 }
+
+// 批量插入
 
 func createMany(db *gorm.DB) {
 	users := []*model.User{
 		{Account: "1", Password: "1"},
 		{Account: "2", Password: "2"},
 		{Account: "3", Password: "3"},
+		{Account: "4", Password: "4"},
 	}
-	result := db.Create(users)
-	fmt.Printf("%+v", result)
+	db = db.Session(&gorm.Session{CreateBatchSize: 4})
+	result := db.Create(&users)
+	for _, user := range users {
+		fmt.Printf("%d ", user.ID)
+	}
+	fmt.Printf("%+v\n", result)
 }
 
 // 创建指定列的数据
@@ -51,4 +63,31 @@ func createBySomeFields(db *gorm.DB) {
 	}
 	db.Select("account").Create(user)
 	fmt.Printf("%+v", user)
+}
+
+// 创建数据时使用钩子
+
+func createUseHook(db *gorm.DB) {
+	users := []*model.User{
+		{Account: "1", Password: "1"},
+		{Account: "2", Password: "2"},
+		{Account: "3", Password: "3"},
+		{Account: "4", Password: "4"},
+	}
+	fmt.Printf("createUseHook-skipHooks")
+	db.Session(&gorm.Session{SkipHooks: true}).Create(&users)
+}
+
+// 创建数据通过map
+
+func createByMap(db *gorm.DB) {
+	db.Model(&model.User{}).Create(map[string]interface{}{
+		"Account": "jinzhu", "Password": 18,
+	})
+
+	// batch insert from `[]map[string]interface{}{}`
+	db.Model(&model.User{}).Create([]map[string]interface{}{
+		{"Account": "jinzhu_1", "Password": "18"},
+		{"Account": "jinzhu_2", "Password": "20"},
+	})
 }
--- gorm-study/model/user.go
+++ gorm-study/model/user.go
@@ -1,9 +1,17 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Account  string
 	Password string
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	fmt.Printf("BeforeCreate-%+v", u)
+	return nil
 }
